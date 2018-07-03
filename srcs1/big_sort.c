d3c36204600fd21d342ffda844a0b151b90ee767
blee3285 <benlee3285@gmail.com>
2018-07-03 03:31:46 -0700

    still working on big sorts

--- srcs1/big_sort.c
+++ srcs1/big_sort.c
@@ -6,15 +6,51 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/03 17:28:35 by blee              #+#    #+#             */
-/*   Updated: 2018/06/29 17:35:29 by blee             ###   ########.fr       */
+/*   Updated: 2018/07/03 02:41:34 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rot_back(t_num **lst_a, t_num **lst_b, int x, char ab)
+void	push_to_other(t_num **lst_a, t_num **lst_b, int min, int max, char ab)
 {
+	int	mid;
+	int	i;
 
+	mid = (min + max) / 2;
+	i = min;
+	if (ab == 'a')
+		while (i++ <= max)
+			if ((*lst_a)->order < mid)
+				ps_pb(lst_a, lst_b);
+			else
+				ps_ra(lst_a, lst_b);
+	else if (ab == 'b')
+		while (i++ <= max)
+			if ((*lst_b)->order >= mid)
+				ps_pa(lst_a, lst_b);
+			else
+				ps_rb(lst_a, lst_b);
+}
+
+void	rot_back(t_num **lst_a, t_num **lst_b, int len, char ab)
+{
+	if (ab == 'a')
+	{
+		while (len)
+		{
+			ps_rra(lst_a, lst_b);
+			len--;
+		}
+	}
+	else if (ab == 'b')
+	{
+		while (len)
+		{
+			ps_rrb(lst_a, lst_b);
+			len--;
+		}
+	}
 }
 
 void	quick_sort_b(t_num **lst_a, t_num **lst_b, int min, int max)
@@ -23,15 +59,24 @@ void	quick_sort_b(t_num **lst_a, t_num **lst_b, int min, int max)
 	int		i;
 
 	mid = (min + max) / 2;
-	i = 0;
-	while (i < (max - mid))
+	i = min;
+	if ((min - max) == 1)
 	{
-		if ((*lst_b)->order >= mid)
-			ps_pa(lst_a, lst_b);
-		else
-			ps_rb(lst_a, lst_b);
-		i++;
+		ps_pa(lst_a, lst_b);
+		return ;
 	}
+	else if ((min - max) == 2)
+	{
+		if ((*lst_b)->order > (*lst_b)->next->order)
+			ps_sb(lst_a, lst_b);
+		while ((*lst_b))
+			ps_pa(lst_a, lst_b);
+		return ;
+	}
+	push_to_other(lst_a, lst_b, min, max, 'b');
+	rot_back(lst_a, lst_b, (mid - min), 'b');
+	quick_sort_a(lst_a, lst_b, mid, max);
+	quick_sort_b(lst_a, lst_b, min, (mid - 1));
 }
 
 void	quick_sort_a(t_num **lst_a, t_num **lst_b, int min, int max)
@@ -40,7 +85,7 @@ void	quick_sort_a(t_num **lst_a, t_num **lst_b, int min, int max)
 	int		i;
 
 	mid = (min + max) / 2;
-	i = 0;
+	i = min;
 	if ((max - min) < 2)
 		return ;
 	else if ((max - min) < 3)
@@ -49,15 +94,8 @@ void	quick_sort_a(t_num **lst_a, t_num **lst_b, int min, int max)
 			ps_sa(lst_a, lst_b);
 		return ;
 	}
-	while (i < (max - min))
-	{
-		if ((*lst_a)->order < mid)
-			ps_pb(lst_a, lst_b);
-		else
-			ps_ra(lst_a, lst_b);
-		i++;
-	}
-	//if not the first call, rotate back the numbers you didn't push
+	push_to_other(lst_a, lst_b, min, max, 'a');
+	rot_back(lst_a, lst_b, )
 	quick_sort_a(lst_a, lst_b, mid, max);
 	quick_sort_b(lst_a, lst_b, min, (mid - 1));
 }
